89b5b91e8c3e56c40d226337cbaf8a14e3d3211f
Roger Zhang <1787009521@qq.com>
2022-04-01 10:02:00 +0800

    class封装tab组件

--- self/tab/js/index.js
+++ self/tab/js/index.js
@@ -1,10 +1,18 @@
-$(function() {
-    $('.tabs').each(function(index, element) {
-        $(element).find('ol').eq(0).on('click', 'li', function(e) {
-          var _index = $(this).index()
-          $(e.currentTarget).addClass('active').siblings().removeClass('active')
-          $(element).find('ol').eq(1).find('li').eq(_index).addClass('active').siblings().removeClass('active')
-          console.log($(element).find('ol').eq(1).find('li').eq(_index).html())
-        })
+
+class Tabs {
+  constructor(selector) {
+    this.selector = $(selector)
+    this.init()
+  }
+  init() {
+    this.selector.find('ol').eq(0).find('li').eq(0).addClass('active')
+    this.selector.find('ol').eq(1).find('li').eq(0).addClass('active')
+    this.selector.each(function(index, element) {
+      $(element).find('ol').eq(0).on('click', 'li', function(e) {
+        var _index = $(this).index()
+        $(e.currentTarget).addClass('active').siblings().removeClass('active')
+        $(element).find('ol').eq(1).find('li').eq(_index).addClass('active').siblings().removeClass('active')
       })
-})
+    })
+  }
+}
